ee2dc6340f40d461b2a0461959f7eb1d4bac0fe5
Redha Gouicem <redha.gouicem@gmail.com>
2022-09-01 16:58:59 +0200

    download phoenix prebuilt binaries instead of compiling

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,6 @@
 # Build qemu
 nix-shell --run ${RISOTTO_ROOT}/scripts/build_qemu.sh ${RISOTTO_ROOT}/default.nix
 
-# Build/download benchmarks
+# Download benchmarks binaries
 nix-shell --run ${RISOTTO_ROOT}/scripts/build_benchmarks.sh ${RISOTTO_ROOT}/default.nix
 
--- scripts/build_benchmarks.sh
+++ scripts/build_benchmarks.sh
@@ -1,14 +1,13 @@
 #!/usr/bin/env bash
 
-cd benchmarks
+cd ${RISOTTO_ROOT}/benchmarks
 
 # parsec
 wget https://nextcloud.in.tum.de/index.php/s/AFBrjRbsHkcgF7b/download/parsec-bins.tar.xz
 tar xf parsec-bins.tar.xz
 
 # phoenix
-cd phoenix/phoenix-2.0
-make
+wget https://nextcloud.in.tum.de/index.php/s/bZiqieCkr782QWG/download/phoenix-bins.tar.xz
+tar xf phoenix-bins.tar.xz
 
-cd -
-
+cd ${RISOTTO_ROOT}
