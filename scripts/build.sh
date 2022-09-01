570700f9719008e1b2b6ce8c6b7e64ba3c631f24
Redha Gouicem <redha.gouicem@gmail.com>
2022-09-01 16:33:36 +0200

    add parsec download in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,3 +2,9 @@
 
 # Build qemu
 nix-shell --run ${RISOTTO_ROOT}/scripts/build_qemu.sh ${RISOTTO_ROOT}/default.nix
+
+# Download benchmarks
+cd benchmarks
+wget https://nextcloud.in.tum.de/index.php/s/AFBrjRbsHkcgF7b/download/parsec-bins.tar.xz
+tar xf parsec-bins.tar.xz
+cd -
